69b691079a8f6de43cb418b8e5fd5b87b997ba73
Mohammad Ali Bagheri <ma.bagheri@gmail.com>
2024-11-21 09:56:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ top_100_stocks = [
 
 # Fetch data (cached)
 # df = fetch_stock_data(top_100_stocks)
->>> df = pd.DataFrame({'num_legs': [2, 4, 8, 0],
+df = pd.DataFrame({'num_legs': [2, 4, 8, 0],
                     'num_wings': [2, 0, 0, 0],
                     'num_specimen_seen': [10, 2, 1, 8]},
                    index=['falcon', 'dog', 'spider', 'fish'])
