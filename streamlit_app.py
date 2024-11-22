251a3fea72d7d340edab7cd46f7fa6661b42e784
Mohammad Ali Bagheri <ma.bagheri@gmail.com>
2024-11-21 20:45:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def fetch_stock_data(tickers):
         # Extract desired metrics
         data.append({
             " ": ticker,
-            "MarCap (B)": np.round(info.get("marketCap") / 1000000, 2),
+            "MarCap (B)": info.get("marketCap") / 1e9 if info.get("marketCap") else None,
             "Price": info.get("regularMarketPrice"),
             # "Chg (%)": info.get("regularMarketChangePercent"),
             "Volume": info.get("regularMarketVolume"),
